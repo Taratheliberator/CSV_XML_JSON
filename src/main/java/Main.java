1baf7745aed18150cf95fa2619bf8ab0cc15c179
Viner Gindullin <Taratheliberator@gmail.com>
2022-10-02 13:29:27 +0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,96 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+
+        List<Employee> staff = null;
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            staff = csv.parse();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return staff;
+    }
+
+    public static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        return gson.toJson(list, listType);
+    }
+
+    public static void writeString(String str, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(str);
+            writer.append("\n");
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static List<Employee> parseXML(String fileName) {
+        List<Employee> staff = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(fileName));
+            Node root = doc.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (Node.ELEMENT_NODE == node.getNodeType()) {
+                    Element element = (Element) node;
+                    Employee employee = new Employee(Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent()),
+                            element.getElementsByTagName("firstName").item(0).getTextContent(),
+                            element.getElementsByTagName("lastName").item(0).getTextContent(),
+                            element.getElementsByTagName("country").item(0).getTextContent(),
+                            Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent()));
+                    staff.add(employee);
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return staff;
+    }
+
+    public static void main(String[] args) {
+
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list1 = parseCSV(columnMapping, fileName);
+        String json1 = listToJson(list1);
+        writeString(json1, "data1.json");
+        List<Employee> list2 = parseXML("data.xml");
+        String json2 = listToJson(list2);
+        writeString(json2, "data2.json");
+    }
+}
